3b7a0345d62a6f13d5b2480bb645e41c1bded67a
Elias Johnstone <eliasj@student.chalmers.se>
2014-08-09 13:12:25 +0200

    fix the version number

--- pbap/__init__.py
+++ pbap/__init__.py
@@ -2,7 +2,7 @@
 
 from pbap import *
 
-__version__ = 0.1.1
+__version__ = "0.1.1"
 __author__ = "Elias Johnstone"
 __email__ = "eliasj@student.chalmers.se"
 __url__ = "https://github.com/eliasj/pbap"
